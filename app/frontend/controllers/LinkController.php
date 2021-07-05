5be6eda72ed94bec285fddaf3ba520d050d0c76e
Semenykhin Maksym <>
2021-07-05 20:00:24 +0300

    <bug>Show only user links

--- app/frontend/controllers/LinkController.php
+++ app/frontend/controllers/LinkController.php
@@ -75,7 +75,7 @@ class LinkController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new LinkSearch();
+        $searchModel = new LinkSearch(['user'=>\yii::$app->user->id]);
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
